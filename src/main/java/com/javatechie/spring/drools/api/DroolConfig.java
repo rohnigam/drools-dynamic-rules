9e39b7a6cd7f1de20ec860527b4bb8fed9a2a613
Rohitashwa Nigam <rohitashwa.nigam@oracle.com>
2022-09-15 09:49:31 +0530

    Test out creating Kie Container from a drl file at run time

--- src/main/java/com/javatechie/spring/drools/api/DroolConfig.java
+++ src/main/java/com/javatechie/spring/drools/api/DroolConfig.java
@@ -17,41 +17,41 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DroolConfig {
 
-	private KieServices kieServices = KieServices.Factory.get();
-
-	private KieFileSystem getKieFileSystem() throws IOException {
-		KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
-		kieFileSystem.write(ResourceFactory.newClassPathResource("offer.drl"));
-		return kieFileSystem;
-
-	}
-
-	@Bean
-	public KieContainer getKieContainer() throws IOException {
-		System.out.println("Container created...");
-		getKieRepository();
-		KieBuilder kb = kieServices.newKieBuilder(getKieFileSystem());
-		kb.buildAll();
-		KieModule kieModule = kb.getKieModule();
-		KieContainer kContainer = kieServices.newKieContainer(kieModule.getReleaseId());
-		return kContainer;
-
-	}
-
-	private void getKieRepository() {
-		final KieRepository kieRepository = kieServices.getRepository();
-		kieRepository.addKieModule(new KieModule() {
-			public ReleaseId getReleaseId() {
-				return kieRepository.getDefaultReleaseId();
-			}
-		});
-	}
-
-	@Bean
-	public KieSession getKieSession() throws IOException {
-		System.out.println("session created...");
-		return getKieContainer().newKieSession();
-
-	}
+//	private KieServices kieServices = KieServices.Factory.get();
+//
+//	private KieFileSystem getKieFileSystem() throws IOException {
+//		KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
+//		kieFileSystem.write(ResourceFactory.newClassPathResource("rulesConfigs/default.drl"));
+//		return kieFileSystem;
+//
+//	}
+//
+//	@Bean
+//	public KieContainer getKieContainer() throws IOException {
+//		System.out.println("Container created...");
+//		getKieRepository();
+//		KieBuilder kb = kieServices.newKieBuilder(getKieFileSystem());
+//		kb.buildAll();
+//		KieModule kieModule = kb.getKieModule();
+//		KieContainer kContainer = kieServices.newKieContainer(kieModule.getReleaseId());
+//		return kContainer;
+//
+//	}
+//
+//	private void getKieRepository() {
+//		final KieRepository kieRepository = kieServices.getRepository();
+//		kieRepository.addKieModule(new KieModule() {
+//			public ReleaseId getReleaseId() {
+//				return kieRepository.getDefaultReleaseId();
+//			}
+//		});
+//	}
+//
+//	@Bean
+//	public KieSession getKieSession() throws IOException {
+//		System.out.println("session created...");
+//		return getKieContainer().newKieSession();
+//
+//	}
 
 }
--- src/main/java/com/javatechie/spring/drools/api/MegaOfferController.java
+++ src/main/java/com/javatechie/spring/drools/api/MegaOfferController.java
@@ -1,6 +1,18 @@
 package com.javatechie.spring.drools.api;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieBuilder;
+import org.kie.api.builder.KieFileSystem;
+import org.kie.api.builder.KieModule;
+import org.kie.api.builder.KieRepository;
+import org.kie.api.builder.ReleaseId;
+import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+import org.kie.internal.io.ResourceFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,14 +20,67 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MegaOfferController {
-	@Autowired
-	private KieSession session;
+//	@Autowired
+//	private KieSession session;
 
 	@PostMapping("/order")
 	public Order orderNow(@RequestBody Order order) {
-		session.insert(order);
-		session.fireAllRules();
+
+
+//		session.insert(order);
+//		session.fireAllRules();
+
+		KieSession customSession = getCustomSessionForClient(order.getClientType());
+
+		customSession.insert(order);
+		customSession.fireAllRules();
 		return order;
+	}
+
+	private KieSession getCustomSessionForClient(String clientType) {
+		String filename = getRulesFileForClientType(clientType);
+
+		KieServices kieServices = KieServices.Factory.get();
+		KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
+//		kieFileSystem.write(ResourceFactory.newClassPathResource(filename));
+		kieFileSystem.write(ResourceFactory.newFileResource(filename));
+		System.out.println("Container created...");
+		getKieRepository(kieServices);
+		KieBuilder kb = kieServices.newKieBuilder(kieFileSystem);
+		kb.buildAll();
+		KieModule kieModule = kb.getKieModule();
+		KieContainer kContainer = kieServices.newKieContainer(kieModule.getReleaseId());
+		KieSession customSession = kContainer.newKieSession();
+
+		return customSession;
+	}
+
+	private String getRulesFileForClientType(String clientType) {
+		String filename = "";
+
+		try {
+			InputStream getLocalJsonFile = new FileInputStream(
+				"/Users/rohitashwanigam/repos/mine/drools-dynamic-rules/rulesConfigForClient.json");
+			HashMap<String,String> rulesConfigForClient = new ObjectMapper().readValue(getLocalJsonFile, HashMap.class);
+			if (rulesConfigForClient.containsKey(clientType)) {
+				filename = rulesConfigForClient.get(clientType);
+			} else {
+				filename = rulesConfigForClient.get("default");
+			}
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+
+		return filename;
+	}
+
+	private void getKieRepository(KieServices kieServices) {
+		final KieRepository kieRepository = kieServices.getRepository();
+		kieRepository.addKieModule(new KieModule() {
+			public ReleaseId getReleaseId() {
+				return kieRepository.getDefaultReleaseId();
+			}
+		});
 	}
 
 }
